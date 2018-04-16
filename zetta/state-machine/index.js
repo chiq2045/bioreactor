828bc6203109635c986c08a7aedad10fadf7ed8d
Chiq2045 <charles.oroko@outlook.com>
2018-04-16 11:49:38 -0700

    Remove ignored directories

--- zetta/state-machine/index.js
+++
@@ -1,9 +0,0 @@
-var zetta = require('zetta');
-var stateMachineDevice = require('./device.js');
-
-zetta()
-  .name('State Machine Zetta')
-  .use(stateMachineDevice)
-  .listen(1337, function(){
-      console.log('Zetta is running at http://127.0.0.1:1337');
-});
