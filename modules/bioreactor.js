261f84d29b0df82529badc9c5147c2cae7978548
Charles Oroko <charles.oroko@outlook.com>
2018-04-16 23:23:06 -0700

    Updated bioreactor.js

--- modules/bioreactor.js
+++ modules/bioreactor.js
@@ -8,8 +8,8 @@ wifi.disconnect();
 
 var i2c = new I2C();
 i2c.setup({scl:NodeMCU.D2, sda:NodeMCU.D1});
-var isp = new ISP();
-isp.setup({miso:NodeMCU.D5, sck:NodeMCU.D7});
+var spi = new SPI();
+spi.setup({miso:NodeMCU.D5, sck:NodeMCU.D7});
 var cs = NodeMCU.D6;
 
 var reg = {
@@ -23,14 +23,10 @@ var reg = {
   txlvl      : 0x08<<3,
   rxlvl      : 0x09<<3
 };
+
 var readPPM = [0xFF,0x01,0x9C,0x00,0x00,0x00,0x00,0x00,0x63];
-var ph = 0.0;
-var temp = 0.0;
-var co2 = 0;
-var data = [];
 
-
-function phComm(comm) {
+function phComm(i2c, comm) {
   i2c.writeTo(0x63, comm);
   var phData = [];
   var str = [];
@@ -47,18 +43,54 @@ function phComm(comm) {
   return str.join("");
 }
 
-function co2Begin() {
-  i2c.writeTo(0x4d, [iocontrol, 0x08]);
-  i2c.writeTo(0x4d, [fcr, 0x07]);
-  i2c.writeTo(0x4d, [lcr, 0x83]);
-  i2c.writeTo(0x4d, [dll, 0x60]);
-  i2c.writeTo(0x4d, [dlh, 0x00]);
-  i2c.writeTo(0x4d, [lcr, 0x03]);
+function co2Begin(reg) {
+  i2c.writeTo(0x4d, [reg.iocontrol, 0x08]);
+  i2c.writeTo(0x4d, [reg.fcr, 0x07]);
+  i2c.writeTo(0x4d, [reg.lcr, 0x83]);
+  i2c.writeTo(0x4d, [reg.dll, 0x60]);
+  i2c.writeTo(0x4d, [reg.dlh, 0x00]);
+  i2c.writeTo(0x4d, [reg.lcr, 0x03]);
 }
 
-function co2Read() {
-  i2c.writeTo(0x4d, [fcr, 0x07]);
-  i2c.writeTo(0x4d, txlvl);
+function co2Read(reg, readPPM) {
+  i2c.writeTo(0x4d, [reg.fcr, 0x07]);
+  i2c.writeTo(0x4d, reg.txlvl);
   if (i2c.readFrom(0x4d, 1) >= readPPM.length) {
-    i2c.write(0x4d, [thr, 0x07]);
+    i2c.write(0x4d, [reg.thr, readPPM]);
+  }
+  setTimeout(function() {
+    var rx = 0;
+    var n = 9;
+    var buf = [];
+    while (n>0) {
+      i2c.writeTo(0x4d, reg.rxlvl);
+      rx = i2c.readFrom(0x4d, 1);
+
+      if (rx>n) {
+        rx = n;
+      }
+
+      i2c.writeTo(0x4d, reg.rhr);
+      buf.push(i2c.readFrom(0x4d, rx));
+      n = n-rx;
+    }
+  }, 1);
+  return buf;
+}
+
+function tempC(spi) {
+  var d = spi.send([0,0,0,0], cs);
+  var v = d[0]<<24 | d[1]<<16 | d[2]<<8 | d[3];
+
+  if (v & 0x7) {
+    return NaN;
+  }
+
+  if (v & 0x80000000) {
+    v = 0xffffc000 | ((v>>18) & 0x0003ffff);
+  } else {
+    v>>=18;
+  }
+
+  return v/4;
 }
