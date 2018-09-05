8098b30195e914b7ac0ba3d37855f45b0df41357
Charles Oroko <charles.oroko@outlook.com>
2018-09-04 18:32:13 -0700

    aDD SOME FUNCTIONALITY

--- projects/code.js
+++ projects/code.js
@@ -30,40 +30,6 @@ P = new (require('ezoph'))( i2c, phAddress );
 C = new (require('mh_z16'))( i2c, co2Address );
 T = new (require('max31855k'))( spi, sck, miso, cs );
 
-var co2Timer = setInterval(() => {
-    digitalWrite(actuators.co2, true);
-    console.log("Co2 on");
-    setTimeout(() => {
-        digitalWrite(actuators.co2, false);
-        console.log("Co2 off");
-    }, 5000);
-}, (1000 * 60 * 5));
-
-/*
-const CMDS = {
-  readData: [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-  readCommand: [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79],
-  calCommand: [0xff, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78]
-};
-
-Serial2.setup(9600, {tx: D4, rx: D15});
-
-function sendcommand(arr) {
-	Serial2.write(arr);
-}
-
-function measure() {
-	sendcommand(CMDS.readCommand);
-	
-    console.log(Serial2.available());
-	while(Serial2.available()) {
-		console.log("reading data: ");
-		var result = Serial2.read();
-		console.log(result);
-	}
-}
-
-/*
 C.begin(); // initialize the co2 Sensor
 T.begin(); //initialise temperature sensor
 // measure the CO2 concentration every 10 seconds
@@ -75,9 +41,11 @@ var readPH = setInterval(() => {P.read(() => {});}, 2000);
 //actuators
 var actuators = {
   temp : D19,
-  co2:D18,
-  tempTime:getTime(),
-  co2Time: getTime(),
+  co2  : D21,
+  tempOn: getTime(),
+  tempOff:getTime(),
+  co2On: getTime(),
+  co2Off: getTime(),
   tempStatus : false,
   co2Status : false
 };
@@ -96,33 +64,24 @@ var updateData = setInterval(() => {
 
 /*
 var updateActuators = setInterval(() => {
-  //actuators.temp = actuators.tempStatus;
-  //actuators.co2 = actuators.co2Status;
+  actuators.temp = actuators.tempStatus;
+  actuators.co2 = actuators.co2Status;
 
   if (!actuators.tempStatus
-      && bioData.temp < 36.25
-      && getTime()-actuators.tempTime > 15) {
-    digitalWrite(actuators.temp, true);
+      && bioData.temp < 35
+      && getTime()-actuators.tempOff >=14000) {
+    digitalWrite(D19, true);
     actuators.tempStatus = true;
-    actuators.tempTime= getTime();
+    actuators.tempOn = getTime();
     console.log('on');
   }
   if (actuators.tempStatus
-      && (bioData.temp >= 37
-      || getTime()-actuators.tempTime > 5)) {
+      && getTime()-actuators.tempOn >=7000
+      || bioData.temp >= 36) {
     digitalWrite(D19, false);
     actuators.tempStatus = false;
-    actuators.tempTime = getTime();
+    actuators.tempOff = getTime();
     console.log('off');
   }
 }, 1000);
-analogWrite(D13, .15)
-
-var addNaOH = function() {
- D12.write(false)
- setTimeout(()=> {
-  D12.write(true)
- }, 100)
-}
-
 */
